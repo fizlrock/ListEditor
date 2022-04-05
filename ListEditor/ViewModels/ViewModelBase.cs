21f11f0c91bd894542e823d4b66eab3ce30cfb59
nirku0soft@yandex.ru <O1uD1QqzKMes9R8YEXwq>
2022-04-05 16:06:03 -0400

    Основа проекта

---
+++ ListEditor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace ListEditor.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ ListEditor/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ListEditor.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
