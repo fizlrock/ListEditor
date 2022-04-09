e803843619233519fad97b83fff48db31ae00168
nirku0soft@yandex.ru <O1uD1QqzKMes9R8YEXwq>
2022-04-09 13:19:54 -0400

    Добавление оболочки для списка. Методы для создание списка и просмотра существующих списков.

--- ListEditorConsole/Program.cs
+++ ListEditorConsole/Program.cs
@@ -1,5 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using ListEditor;
 
-Console.WriteLine("Hello, World!");
+public class ListEditorCore
+{
+    private List<CycleSingleLinkedListShell> existing_lists = new List<CycleSingleLinkedListShell>();
+
+    public string[] ListOfLists
+    {
+        get
+        {
+            string[] list_of_lists = new string[existing_lists.Count];
+            for (int i = 0; i < existing_lists.Count; i ++)
+            {
+                list_of_lists[i] = existing_lists[i].Name;
+            }
+            return list_of_lists;
+        }
+    }
+
+    public void CreateEmptyList(string list_name)
+    {
+        CycleSingleLinkedList my_list = new CycleSingleLinkedList();
+        CycleSingleLinkedListShell my_list_shell = new CycleSingleLinkedListShell();
+        my_list_shell.Name = list_name;
+        existing_lists.Append(my_list_shell);
+    }
+
+}
+
+
+class CycleSingleLinkedListShell
+{
+    private string list_name;
+    private CycleSingleLinkedList my_list;
+
+    public string Name
+    {
+        get { return list_name; }
+        set
+        {
+            list_name = value.Trim();
+        }
+    }
+
+    public CycleSingleLinkedList MyList
+    {
+        get { return my_list; }
+        set
+        {
+            
+        }
+    }
+}
