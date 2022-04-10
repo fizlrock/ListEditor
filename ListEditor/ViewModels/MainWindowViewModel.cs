8bafefaadd81e766b399252d886dcf1f9535d7b2
nirku0soft@yandex.ru <O1uD1QqzKMes9R8YEXwq>
2022-04-10 15:13:58 -0400

    Добавленая фукнкция создания пустого списка

--- ListEditor/ViewModels/MainWindowViewModel.cs
+++ ListEditor/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using JetBrains.Annotations;
+using ReactiveUI;
 
-namespace ListEditor.ViewModels
+namespace ListEditor.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
 {
-    public class MainWindowViewModel : ViewModelBase
+    private bool is_selected;
+    public ReactiveCommand<Unit, Unit> CreateListCommand { get; }
+    public ReactiveCommand<Unit, Unit> ExitCommand { get; }
+
+    private string _ListName;
+    private ListEditorCore core;
+    public string ListName
     {
-        public string Greeting => "Привет стёпа!";
+        get { return _ListName;}
+        set { this.RaiseAndSetIfChanged(ref _ListName, value); }
     }
+    
+
+
+    public MainWindowViewModel()
+    {
+        CreateListCommand = ReactiveCommand.Create(CreateList);
+        ExitCommand = ReactiveCommand.Create(Exit);
+
+        core = new ListEditorCore();
+
+    }
+
+    public void CreateList()
+    {
+        Console.WriteLine(ListName);
+        core.CreateEmptyList(ListName);
+        ListName = "";
+    }
+
+    public void Exit()
+    {
+        Environment.Exit(0);
+    }
+
 }
+
+
